3517c4239aedf6dfc1672c415348f3a40a43379b
sumiati04 <ss6289285@gmail.com>
2022-07-07 16:12:28 +0700

    apaya

--- guru/index.php
+++ guru/index.php
@@ -50,7 +50,7 @@
                         <th scope="col">NIP</th>
                         <th scope="col">Nama</th>
                         <th scope="col">Alamat</th>
-                        <th scope="col">Action</th>
+                        <th scope="col">Actionn</th>
                         </tr>
                     </thead>
                     
